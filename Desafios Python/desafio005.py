b9268a64c0746f4a67bc0766197a4ff89fb08b80
Edson Lucas <EDSONLUCASBD94@GMAIL.COM>
2020-07-23 22:29:09 -0300

    Delete desafio005.py

--- Desafios Python/desafio005.py
+++
@@ -1,8 +0,0 @@
-"""Programa que leia dois numeros e mostre na tela o seu sucessor e antecessor."""
-titulo = ' Desafio 05 '
-print('{:=^40}'.format(titulo))
-
-num = int(input('\nDigite um número: '))
-ant = num - 1
-suc = num + 1
-print('\nAntecessor: {}\nSucessor: {}'.format(ant, suc))
